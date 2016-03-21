339b9c4a049966cf8901537706422043f254d829
vchrisb <vchrisb@users.noreply.github.com>
2016-03-21 14:18:20 +0100

    copyright update

--- IsilonPlatform/Functions/IsilonPlatformSet.ps1
+++ IsilonPlatform/Functions/IsilonPlatformSet.ps1
@@ -1,6 +1,6 @@
 # The MIT License
 #
-# Copyright (c) 2015 Christopher Banck.
+# Copyright (c) 2016 Christopher Banck.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
